99f9586053bb84132694343e392b93cc957d7387
camden <me@12oclock.dev>
2021-12-15 23:06:20 -0600

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "dev.twelveoclock"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
